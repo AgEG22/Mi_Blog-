46a12896e5f1018a116e7fdfc5f5fed5622a3cc0
EgAG22 <eliasgomez331@gmial.com>
2022-08-03 18:35:24 -0300

    modificacion 1.0

--- djBlog/settings/base.py
+++ djBlog/settings/base.py
@@ -119,11 +119,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles') 
-]
+    os.path.join(BASE_DIR, 'staticfiles')
+    ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
