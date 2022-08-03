d6e437f8b83c51652529f23131074ef17315cc55
EgAG22 <eliasgomez331@gmial.com>
2022-08-03 16:05:21 -0300

    indicado el uso de development setting en wsgi

--- djBlog/wsgi.py
+++ djBlog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djBlog.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djBlog.settings.development') ### Aqui indico el sttiengs que quiero utilizar, el development.py por defecto
+## El os. permite traer cierta funcionalidades del sistema operativo.                                                                               
 application = get_wsgi_application()
