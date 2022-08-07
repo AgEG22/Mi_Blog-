df440409c8e8b729582191e17e08ca9d917ca68e
AgEG22 <eliasgomez331@gmail.com>
2022-08-07 15:16:52 -0300

    modificaciones deprueba

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # el name=index es darle un nombre a esa url.
+    path('elias', views.index, name='index'), # el name=index es darle un nombre a esa url.
 ]
 
-## Lo que esta entre comilla vacio es la ruta a la que yo quiero que aparezca el "Hola mundo".
+## Lo que esta entre comilla vacio es la ruta a la que yo quiero que aparezca el "Hola mundo" a travez de la funcion index.
 
 ## Aqui configuramos que rutas van a mostrar la vista, definida por la funcion index. 
